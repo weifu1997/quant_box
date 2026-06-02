0a4f5656308ae6d57b16733cfe035a627305c366
weifu1997 <weifu1997@users.noreply.github.com>
2026-06-02 22:15:46 +0800

    Reduce data update sleep interval

--- 02_补齐股票数据_持续.bat
+++ 02_补齐股票数据_持续.bat
@@ -7,7 +7,7 @@ set PYTHON=python
 if exist "%~dp0.venv\Scripts\python.exe" set PYTHON=%~dp0.venv\Scripts\python.exe
 
 set CHUNK_SIZE=50
-set SLEEP_SECONDS=60
+set SLEEP_SECONDS=30
 
 echo Starting resumable stock data backfill...
 echo Chunk size: %CHUNK_SIZE%
